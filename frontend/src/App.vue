79166523abcd3625832d895ece03891543a5f706
Manas <imanaspaul@gmail.com>
2020-07-29 13:26:38 +0530

    Thenav css removed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,7 +22,26 @@ body{
 p{
     font-size: 20px;
 }
-
+.nav-item{
+    margin-right:30px;
+    
+}
+.active{
+    transition: cubic-bezier(0.075, 0.82, 0.165, 1);
+}
+.nav-link:hover{
+    color: grey!important;
+}
+.router-link-exact-active {
+    border: 1.5px solid red;
+}
+.nav-item:last-child{
+    margin-right: 0px;
+}
+.nav-link{
+  font-size: 11px!important;
+  font-weight: 500!important;
+}
 @media only screen and (max-width: 1500px) {
   .nav-item{
     margin-right:0px;
